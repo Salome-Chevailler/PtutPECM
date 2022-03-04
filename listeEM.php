882afc23ac8307823107b5a795b463bfe4bf7a5c
Salome-Chevailler <salome.chevailler@etud.univ-jfc.fr>
2022-03-04 16:16:08 +0100

    Affichage de tous les événements dans un tableau

--- listeEM.php
+++ listeEM.php
@@ -6,7 +6,7 @@
 <!DOCTYPE html> 
 <html lang="fr">
     <head>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <link rel="stylesheet" href="bootstrap.min.css" type="text/css" media="screen">
 		<link rel="stylesheet" href="bootstrap.css" type="text/css" media="screen">
         <title>Recueil des événements</title>
@@ -24,8 +24,6 @@
             <table class="table table-striped table-sm mb-4">
                 <thead>
                     <tr>
-                    <form method="POST">
-                        <th class="col-md-1">Numéro <input class="videB" type="submit" name="triNumero" value="v"/></th>
                         <th class="col-md-1">Date <input class="videB" type="submit" name="triDate" value="v"/></th>
                         <th class="col-md-1">Service <input class="videB" type="submit" name="triService" value="v"/></th>
                         <th class="col-md-2">Patient à risque <input class="videB" type="submit" name="triPatient" value="v"/></th>
@@ -33,11 +31,70 @@
                         <th class="col-md-3">Voie d'administration à risque <input class="videB" type="submit" name="triAdministration" value="v"/></th>
                         <th class="col-md-3">Description</th>
                         <th class="col-md-2">Consulter/Analyser</th>
-                    </form>
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                        // Récupération des données à afficher dans le tableau
+                        $sql = "SELECT numero, d.nom as departement, date_EM, patient_risque, medicament_risque, administration_risque, details, est_analyse FROM evenement e JOIN departement d ON e.departement=d.id";
+                        $params = array();
+                        $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
+                        $stmt = sqlsrv_query( $conn, $sql, $params, $options);
+                        if( $stmt === false ) {
+                            die( print_r( sqlsrv_errors(), true));
+                        }
+                        $row_count = sqlsrv_num_rows( $stmt );
+                        if ($row_count === false){
+                            echo "Error in retrieveing row count.";
+                        }
+                        // Pour chaque événement, on met ses données dans une variable, qu'on vient afficher dans la case du tableau associée   
+                        for ($i=0; $i<$row_count; $i++){
+                            if( sqlsrv_fetch( $stmt ) === false) {
+                                die( print_r( sqlsrv_errors(), true));
+                            }
+                            $numero = sqlsrv_get_field( $stmt, 0);
+                            $_SESSION['numero']=$numero;
+                            $departement = sqlsrv_get_field( $stmt, 1);
+                            $date = sqlsrv_get_field( $stmt, 2)->format('d/m/Y');
+                            $patient_risque = sqlsrv_get_field( $stmt, 3);
+                            // On transforme le bit en string 
+                            if ($patient_risque===0){
+                                $patient_risque = "Non";
+                            } else {
+                                $patient_risque = "Oui";
+                            }
+                            $medicament_risque = sqlsrv_get_field( $stmt, 4);
+                            if ($medicament_risque===0){
+                                $medicament_risque = "Non";
+                            } else {
+                                $medicament_risque = "Oui";
+                            }
+                            $administration_risque = sqlsrv_get_field( $stmt, 5);
+                            if ($administration_risque===0){
+                                $administration_risque = "Non";
+                            } else {
+                                $administration_risque = "Oui";
+                            }
+                            $details = sqlsrv_get_field( $stmt, 6);
+                            $analyse = sqlsrv_get_field($stmt, 7);
 
+                            echo '<tr>';
+                            echo "<td>$date</td>";
+                            echo "<td>$departement</td>";
+                            echo "<td>$patient_risque</td>";
+                            echo "<td>$medicament_risque</td>";
+                            echo "<td>$administration_risque</td>";
+                            echo "<td>$details</td>";
+                            // Si l'événement n'a jamais été analysé, on affiche un bouton menant à l'analyse
+                            if ($analyse==0){
+                                // On récupère le numéro de l'événement dans l'URL
+                                echo '<td><a href="analyseEM.php?numero='.$numero.'"><input class="boutonthird" type="submit" value="Analyser"><a href="consultationEM.php?numero='.$numero.'"><input class="boutonthird" type="submit" value="Consulter"></td>';
+                            } else {
+                                echo '<td><a href="consultationEM.php?numero='.$numero.'"><input class="boutonthird" type="submit" value="Consulter"></td>';
+                            }
+                            echo '</tr>';
+                        }
+                    ?>
                 </tbody>
             </table>
         </div>
